39bc0bf142619f397e60004a3e7153890c9d45bc
Tobi <showgame360@gmail.com>
2021-09-01 00:23:23 +0100

    #updated

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-docute.init({
-  title: 'PizzleJs',
-  'edit-text': 'Edit this page',
-  'edit-link': 'https://github.com/pizzlejs/pizzlejs.github.io/tree/master/',
-  nav: [
-    { title: 'Home', path: '/' },
-    { title: 'Github', path: 'https://github.com/pizzlejs/' }
-    ],
-  layout: 'narrow'
-});
+hljs.initHighlightingOnLoad();
+
+var animate = new pizzle.create();
+animate.init()
