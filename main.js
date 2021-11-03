7dc5383bade8a88dcd01bf5a0bf3da9b7d86d626
Tobi <showgame360@gmail.com>
2021-11-03 18:57:37 +0100

    Updated

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ hljs.initHighlightingOnLoad();
 
 var animate = new pizzle.create();
 animate.init()
+
+customElements.define('date-now',class extends HTMLElement {
+  constructor(){
+    super();
+  
+    this.innerHTML = new Date().getFullYear();
+  }
+})
